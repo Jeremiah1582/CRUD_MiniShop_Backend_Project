8cef08b04f59885f0e32d96c3f0cf73a9e6227ce
DriesVerb <verboven.dries@gmail.com>
2021-04-29 14:52:23 +0200

    created the basis for our server

--- server.js
+++ server.js
@@ -3,3 +3,25 @@
 
 // Jeremiah test
 // Stella Test
+
+const express = require("express");
+const app = express();
+
+// routers
+const indexRouter = require("./routes/indexRouter");
+
+// settings
+app.set("view engine", "hbs");
+app.use(express.static(`${__dirname}/public`));
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
+// route
+app.use("/", indexRouter);
+
+app.listen(5555, () => {
+  console.log(`server is running`);
+});
